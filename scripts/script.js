c54a9df1f9f29af4aaf7ca443293b57132668464
Jesus Salinas <jesussalinas@MacBook-Pro-de-Clara.local>
2021-12-06 21:26:46 -0600

    initial setup

---
+++ scripts/script.js
@@ -0,0 +1,21 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+import { URL } from 'https://jslib.k6.io/url/1.0.0/index.js';
+
+export default function() {
+  let url;
+  const params = { headers: { 'Authorization': `Bearer ${__ENV.K6_TDO_TOKEN}` } };
+  url = new URL('https://api.todoist.com/rest/v1/projects');
+  const res = http.get(url.toString(), params);
+  check(res, { 'Status Response was 200': (r) => r.status == 200 });
+  console.log('[Cards] Response time was ' + String(res.timings.duration) + ' ms');
+  sleep(1);
+}
+
+export function handleSummary(data) {
+    console.log('Preparing the tests summary...');
+    return {
+        './reports/report.json': JSON.stringify(data)
+    };
+}
+
